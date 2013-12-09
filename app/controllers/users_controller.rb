6986fdbf1fa23cf0ac5e7066fa09088b3460e531
Tina Chen <cchen0616@gmail.com>
2013-12-08 16:40:48 -0800

    inital commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
-  def new
-  end
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			flash[:notice] = 'You had successfully created your account!'
+			session[:user_id] = @user.id
+			redirect_to dashboard_url
+		else
+			render 'new'
+		end
+	end
+
+
+	private
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+	end
 end
